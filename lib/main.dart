007d06cfc4426748f5e443445f28366a7cd3b0e8
slimbde <slimbde@gmail.com>
2025-02-06 19:52:50 +0500

    material-app

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-      child: Text(
-        'Hello flutter',
-        textDirection: TextDirection.ltr,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.red,
-          fontWeight: FontWeight.bold,
-          fontSize: 50,
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white30,
+        appBar: AppBar(
+          title: Text('My first app'),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Text(
+            'This is my app body',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
         ),
       ),
     ),
